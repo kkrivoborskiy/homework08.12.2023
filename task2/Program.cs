acf902f6d7dcc92b361a92d134da1d68618760af
Kirill <kku23102001@gmail.com>
2023-12-08 15:19:24 +0300

    second task finished

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,3 +2,16 @@
 // Напишите программу, которая определяет
 // количество чётных чисел в массиве.
 
+int[] array = new int[10];
+int isFind = 0;
+for (int i=0; i<array.Length; i++)
+    {   
+        array[i] = new Random().Next(1,21);
+        if ( (array[i]%2) == 0)
+            {
+                isFind++;
+            }
+        Console.Write(array[i]+" ");
+    }
+Console.WriteLine("");
+Console.Write(isFind);
