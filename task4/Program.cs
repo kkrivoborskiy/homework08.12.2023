9c1716039adf838b1bc65c79eda1408ac7b53bc3
Kirill <kku23102001@gmail.com>
2023-12-08 16:51:06 +0300

    task4 finished

--- task4/Program.cs
+++ task4/Program.cs
@@ -3,3 +3,22 @@
 // Старший разряд числа должен располагаться на 0-м индексе массива,
 // младший – на последнем. Размер массива должен быть равен количеству цифр.
 
+Console.WriteLine("Введите число от 1 до 100 000");
+int number = Convert.ToInt32(Console.ReadLine());
+int numeralCount = 0;
+int num = number;
+string output = "";
+while (num>0)
+{
+    num = num/10;
+    numeralCount++;
+}
+int[] array = new int[numeralCount];
+Console.Write("[ ");
+for (int i=(numeralCount-1); i>=0; i--)
+    {
+        array[i] = number % 10;
+        number/=10;
+        output = array[i] + " " + output;
+    }
+ Console.Write(output+"]");
