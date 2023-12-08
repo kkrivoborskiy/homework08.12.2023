3aa3fe194283edabb77c5e589351707235380766
Kirill <kku23102001@gmail.com>
2023-12-08 15:13:51 +0300

    task1 finished

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,3 +1,17 @@
 ﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100.
 // Найдите количество элементов массива,
 // значения которых лежат в отрезке [20,90].
+
+int[] array = new int[10];
+int isFind = 0;
+for (int i=0; i<array.Length; i++)
+    {   
+        array[i] = new Random().Next(1,101);
+        if (array[i] > 19 && array[i] < 91)
+            {
+                isFind++;
+            }
+        Console.Write(array[i]+" ");
+    }
+Console.WriteLine("");
+Console.Write(isFind);
